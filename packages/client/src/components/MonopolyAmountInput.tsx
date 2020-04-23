61faa3fa6f65f357e072e3ee764d7d485ebbe3e3
Brent Vollebregt <brent.vollebregt@gmail.com>
2020-04-23 22:02:32 +1200

    Move banker M/K values to the edge of the number input

---
+++ packages/client/src/components/MonopolyAmountInput.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { InputGroup, Button } from "react-bootstrap";
+import NumberFormat, { NumberFormatValues } from "react-number-format";
+
+interface IMonopolyAmountInputProps {
+  amount: number | null;
+  setAmount: (amount: number | null) => void;
+  id: string;
+}
+
+const MonopolyAmountInput: React.FC<IMonopolyAmountInputProps> = ({ amount, setAmount, id }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  // When the external amount changes, update the internal
+  useEffect(() => {
+    setInputValue(amount === 0 || amount === null ? "" : `${amount}`);
+  }, [amount]);
+
+  // When the internal amount changes, update the external
+  useEffect(() => {
+    setAmount(inputValue === "" ? null : parseInt(inputValue, 10));
+  }, [inputValue]);
+
+  const multiply = (multiplier: number) => {
+    const value = parseInt(inputValue, 10);
+    if (!isNaN(value)) {
+      setInputValue(`${multiplier * value}`);
+    }
+  };
+
+  return (
+    <InputGroup style={{ display: "grid", gridTemplateColumns: "2fr 7fr 2fr" }}>
+      <InputGroup.Prepend>
+        <Button block variant="warning" onClick={() => multiply(1000000)}>
+          M
+        </Button>
+      </InputGroup.Prepend>
+
+      <NumberFormat
+        allowNegative={false}
+        thousandSeparator={true}
+        prefix="$"
+        id={id}
+        value={inputValue}
+        onValueChange={({ value }: NumberFormatValues) => setInputValue(value)}
+        className="form-control text-center"
+        autoComplete="off"
+      />
+
+      <InputGroup.Append>
+        <Button block variant="primary" onClick={() => multiply(1000)}>
+          K
+        </Button>
+      </InputGroup.Append>
+    </InputGroup>
+  );
+};
+
+export default MonopolyAmountInput;
--- packages/client/src/pages/Bank/ValuePlayerForm.tsx
+++ packages/client/src/pages/Bank/ValuePlayerForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { InputGroup, Button, DropdownButton, Dropdown, Form, ButtonGroup } from "react-bootstrap";
-import NumberFormat, { NumberFormatValues } from "react-number-format";
+import { Button, DropdownButton, Dropdown, Form, ButtonGroup } from "react-bootstrap";
 import { IGameStatePlayer } from "@monopoly-money/game-state";
+import MonopolyAmountInput from "../../components/MonopolyAmountInput";
 
 interface IValuePlayerFormProps {
   label: string;
@@ -17,34 +17,20 @@ const ValuePlayerForm: React.FC<IValuePlayerFormProps> = ({
   onSubmit
 }) => {
   const identifier = label.toLowerCase().replace(" ", "-");
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState<number | null>(null);
   const [selectedPlayer, setSelectedPlayer] = useState<IGameStatePlayer | null>(null);
   const [submitError, setSubmitError] = useState<string | null>(null);
 
-  const multiply = (multiplier: number) => {
-    const value = parseInt(amount, 10);
-    if (!isNaN(value)) {
-      setAmount(`${multiplier * value}`);
-    }
-  };
-
-  const clear = () => {
-    setAmount("");
-    setSelectedPlayer(null);
-    setSubmitError(null);
-  };
-
   const submit = () => {
-    const numericalAmount = parseInt(amount, 10);
-    if (isNaN(numericalAmount)) {
+    if (amount === null) {
       setSubmitError("Please provide an amount");
-    } else if (numericalAmount <= 0) {
+    } else if (amount <= 0) {
       setSubmitError("You must provide sum larger than $0");
     } else if (selectedPlayer === null) {
       setSubmitError("No player is selected");
     } else {
-      onSubmit(parseInt(amount, 10), selectedPlayer.playerId);
-      setAmount("");
+      onSubmit(amount, selectedPlayer.playerId);
+      setAmount(null);
       setSelectedPlayer(null);
       setSubmitError(null);
     }
@@ -52,34 +38,11 @@ const ValuePlayerForm: React.FC<IValuePlayerFormProps> = ({
 
   return (
     <>
-      <label htmlFor={`${identifier}-value`} className="mb-1">
+      <label htmlFor={identifier} className="mb-1">
         {label}
       </label>
 
-      <InputGroup>
-        <NumberFormat
-          allowNegative={false}
-          thousandSeparator={true}
-          prefix="$"
-          id={`${identifier}-value`}
-          value={amount}
-          onValueChange={({ value }: NumberFormatValues) => setAmount(value)}
-          className="form-control"
-          autoComplete="off"
-        />
-
-        <InputGroup.Append>
-          <Button variant="warning" onClick={() => multiply(1000000)}>
-            M
-          </Button>
-          <Button variant="primary" onClick={() => multiply(1000)}>
-            K
-          </Button>
-          <Button variant="danger" onClick={() => clear()}>
-            C
-          </Button>
-        </InputGroup.Append>
-      </InputGroup>
+      <MonopolyAmountInput amount={amount} setAmount={setAmount} id={identifier} />
 
       <ButtonGroup className="mt-1 player-and-submit-group">
         <DropdownButton

b00cbafc8ed81ec0dc115e8088bf42c98216f322
Brent Vollebregt <brent.vollebregt@gmail.com>
2024-10-02 20:02:54 +1300

    Get frontend building with vite

--- packages/client2/vite.config.ts
+++ packages/client2/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Since we are using workspaces, we also need to include files outside of this projects root
+    commonjsOptions: { include: [/packages\/game-state/, /node_modules/] }
+  }
+});
